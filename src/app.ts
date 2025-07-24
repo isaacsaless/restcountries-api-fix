13ab54f5107bc93c9a1c6a9218753fbd02ede143
isaacsaless <isaacnascimentosales@gmail.com>
2025-07-24 19:56:35 -0300

    fix: changed port

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = Fastify();
 app.register(fastifyCors, {origin: "*"});
 app.register(getFixRestCountries);
 
-app.listen({ port: 3000, host:'0.0.0.0' }, (err, address) => {
+app.listen({ port: 4321, host:'0.0.0.0' }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
